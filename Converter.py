b60876e06a6d33c9797d5f1a756c0eeff948067f
Michał Szymański <michalszymanskyy@gmail.com>
2025-11-22 23:33:35 +0100

    Czyszczenie Pliku Wejsciowego

---
+++ Converter.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+# Wczytaj plik CSV
+
+df = pd.read_csv('HVAC_Dynamic_Fuzzy_PID_2017_with_Target.csv')
+
+
+# Kolumny do pominięcia
+
+columns_to_remove = ['Timestamp','HVAC_Efficiency_Class','ISA_Optimization_Score','User_Comfort_Index','Energy_Efficiency_%','Response_Time_s','HVAC_Power_Consumption_kWh','Fuzzy_Adjustment_Factor','Cooling_Heating_Output_C']
+
+
+# Usuń niepotrzebne kolumny
+
+df_filtered = df.drop(columns=columns_to_remove)
+
+
+# Zapisz do pliku TXT bez nagłówków
+
+df_filtered.to_csv('PlikWynikowy.txt', sep=' ', index=False, header=False)
+
+
+print(f"Plik został utworzony! Liczba wierszy: {len(df_filtered)}")
+
+print(f"Zachowane kolumny: {list(df_filtered.columns)}")
